4af69238a0a5637224addccca2df21d76d5ac9b9
Diogo Souza <diogosouzac@gmail.com>
2021-09-12 13:25:55 -0300

    Update main.js

--- vbr/js/main.js
+++ vbr/js/main.js
@@ -113,12 +113,13 @@ $(document).ready(function() {
     // Popups
     $("#our-packages").magnificPopup({
         delegate: "a",
-        removalDelay: 500, //delay removal by X to allow out-animation
+        removalDelay: 100, //delay removal by X to allow out-animation
         callbacks: {
             beforeOpen: function() {
                 this.st.mainClass = this.st.el.attr("data-effect");
             },
         },
+        fixedContentPos: true,
         midClick: true, // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
     });
 
